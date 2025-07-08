14c12f18112454df2cdcf1aceaa107edb3e261ea
ishaan0082 <ishaansahu210@gmail.com>
2025-07-08 18:41:54 +0530

    hel

--- server/ws-instance.js
+++ server/ws-instance.js
@@ -8,7 +8,6 @@ const rooms ={};
 
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
-    
     ws.on('message', function message(data) {
           // console.log('Received: %s', data);
           const message = JSON.parse(data);
